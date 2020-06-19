3178a984eb2118978dc6ffa7c5b2236b5f378206
Gerold Arheilger <garheilger@xantaro.net>
2020-06-19 18:05:42 +0200

    Functional testing further specified

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,16 @@
+"""
+ Functional test to test our simple To-Do-List Web application
+"""
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+        # Edith wonders whether the site will remember her list. The she sees that the
+        # site has generated a unique URL for her -- there is one explanatory text to that effect.
+
+        # She visits that URL - her to-do list is still there
+
+        # Satisfied, she goes back to sleep
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
 
 assert 'Django' in browser.title
